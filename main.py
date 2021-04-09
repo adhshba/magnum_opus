e4248a209443a601f2d95fe780e64b25a7d3b575
adhshba <75373541+adhshba@users.noreply.github.com>
2021-04-09 20:22:33 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
+@app.route('/')
+def func():
+    return 'NICE'
+
+
 def main():
-    app.run()
+    app.run('127.0.0.1', port=8080)
 
 
 if __name__ == '__main__':
