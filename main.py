a816b89ba8539ff4ca8c18c46555938327effdcc
adhshba <75373541+adhshba@users.noreply.github.com>
2021-04-11 17:27:32 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,25 +2,31 @@ from flask import Flask
 from waitress import serve
 from flask import request, redirect
 
-
 app = Flask(__name__)
-
 
 
 @app.route('/')
 def func():
-    return '''<a href='https://oauth.vk.com/authorize?client_id=7820745&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=messages&response_type=token&v=5.103&state=123456'>rrr</a>'''
+    return '''<script type="text/javascript" src="https://vk.com/js/api/openapi.js?168"></script>
+
+<!-- VK Widget -->
+<div id="vk_allow_messages_from_community"></div>
+<script type="text/javascript">
+VK.Widgets.AllowMessagesFromCommunity("vk_allow_messages_from_community", {}, 203859351);
+</script>'''
     # redirect('https://oauth.vk.com/authorize?client_id=7820745&display=page&redirect_uri=htts://magnumopusproject.xyz/dev/Login&scope=messages&response_type=token&v=5.103&state=123456', code=301)
     # return '''<script type="text/javascript" src="https://vk.com/js/api/openapi.js?168"></script>
-#<script type="text/javascript">
-#  VK.init({apiId: 7820625});
-#</script>
 
-#<!-- VK Widget -->
-#<div id="vk_auth"></div>
-#<script type="text/javascript">
+
+# <script type="text/javascript">
+#  VK.init({apiId: 7820625});
+# </script>
+
+# <!-- VK Widget -->
+# <div id="vk_auth"></div>
+# <script type="text/javascript">
 #  VK.Widgets.Auth("vk_auth", {"authUrl":"/dev/Login"});
-#</script>'''
+# </script>'''
 
 @app.route('/ok')
 def ok():
@@ -33,7 +39,7 @@ def logi():
     hash = request.args.get('hash')
     access_token = request.args.get('access_token')
     return (id, hash, access_token)
-   
+
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=5000)
