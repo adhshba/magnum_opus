7ae7f8d9ebc8fb473a2cec1c0f182203bac141de
adhshba <75373541+adhshba@users.noreply.github.com>
2021-04-11 17:31:27 +0400

    Update main.py

--- main.py
+++ main.py
@@ -8,16 +8,6 @@ app = Flask(__name__)
 @app.route('/')
 def func():
     return '''<script type="text/javascript" src="https://vk.com/js/api/openapi.js?168"></script>
-
-<!-- VK Widget -->
-<div id="vk_allow_messages_from_community"></div>
-<script type="text/javascript">
-VK.Widgets.AllowMessagesFromCommunity("vk_allow_messages_from_community", {}, 203859351);
-</script>'''
-    # redirect('https://oauth.vk.com/authorize?client_id=7820745&display=page&redirect_uri=htts://magnumopusproject.xyz/dev/Login&scope=messages&response_type=token&v=5.103&state=123456', code=301)
-    # return '''<script type="text/javascript" src="https://vk.com/js/api/openapi.js?168"></script>
-
-
 # <script type="text/javascript">
 #  VK.init({apiId: 7820625});
 # </script>
@@ -28,6 +18,7 @@ VK.Widgets.AllowMessagesFromCommunity("vk_allow_messages_from_community", {}, 20
 #  VK.Widgets.Auth("vk_auth", {"authUrl":"/dev/Login"});
 # </script>'''
 
+
 @app.route('/ok')
 def ok():
     return ok
@@ -37,8 +28,12 @@ def ok():
 def logi():
     id = request.args.get('uid')
     hash = request.args.get('hash')
-    access_token = request.args.get('access_token')
-    return (id, hash, access_token)
+    return '''<script type="text/javascript" src="https://vk.com/js/api/openapi.js?168"></script>
+<!-- VK Widget -->
+<div id="vk_allow_messages_from_community"></div>
+<script type="text/javascript">
+VK.Widgets.AllowMessagesFromCommunity("vk_allow_messages_from_community", {}, 203859351);
+</script>'''
 
 
 if __name__ == '__main__':
