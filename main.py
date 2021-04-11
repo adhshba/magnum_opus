703905ace1e942319a9aa3990b451b87190ad91b
adhshba <75373541+adhshba@users.noreply.github.com>
2021-04-11 17:34:29 +0400

    Update main.py

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ app = Flask(__name__)
 @app.route('/')
 def func():
     return '''<script type="text/javascript" src="https://vk.com/js/api/openapi.js?168"></script>
-# <script type="text/javascript">
-#  VK.init({apiId: 7820625});
-# </script>
+<script type="text/javascript">
+VK.init({apiId: 7820625});
+</script>
 
-# <!-- VK Widget -->
-# <div id="vk_auth"></div>
-# <script type="text/javascript">
-#  VK.Widgets.Auth("vk_auth", {"authUrl":"/dev/Login"});
-# </script>'''
+<!-- VK Widget -->
+<div id="vk_auth"></div>
+<script type="text/javascript">
+VK.Widgets.Auth("vk_auth", {"authUrl":"/dev/Login"});
+</script>'''
 
 
 @app.route('/ok')
