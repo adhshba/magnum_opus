7798032a75e7ef8084e342073cba30e1b0fc2ebc
adhshba <75373541+adhshba@users.noreply.github.com>
2021-04-12 21:03:55 +0400

    Add files via upload

---
+++ forms/form_user.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class User_reg(FlaskForm):
+    name = StringField('Имя', validators=[DataRequired()])
+    surname = StringField('Фамилия', validators=[DataRequired()])
+    login = StringField('Login', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    repeat_password = PasswordField('Повторите пароль', validators=[DataRequired()])
+    teacher = BooleanField('Я учитель', validators=[DataRequired()])
+    submit = SubmitField('Зарегистрироваться', validators=[DataRequired()])
