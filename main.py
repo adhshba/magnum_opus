2f88590821aba94167746b4409e1601dbfe39b27
arta159 <75035174+arta159@users.noreply.github.com>
2021-04-14 22:38:35 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import hashlib
+
 from flask import Flask
 from waitress import serve
 from forms.form_user import User_reg
+from data import db_session
 from flask import request, redirect, render_template
+from data.users import User
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -11,13 +16,20 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 def func():
     form = User_reg()
     if form.validate_on_submit():
-        if form.password.data != form.repeat_password.data:
+        if form.password.data != form.password_again.data:
             return render_template('register.html', title='Регистрация',
                                    form=form,
                                    message="Пароли не совпадают")
         # проверка уникальности логина
+        user = User()
+        user.name = form.name.data
+        user.login = form.login.data
+        user.teacher = form.teacher.data
+        user.hashed_password = hashlib.sha512(form.password.data.encode('utf-8')).hexdigest()
+        print(form.password.data)
     return render_template("register.html", form=form, title='Регистрация')
 
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=5000)
+    db_session.global_init("db/school.db")
+    app.run(host='127.0.0.1', port=9998)
