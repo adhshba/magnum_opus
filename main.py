f22cee82c3543421cfc89cc00ffc21e83b853060
adhshba <75373541+adhshba@users.noreply.github.com>
2021-04-15 18:13:08 +0400

    Update main.py

--- main.py
+++ main.py
@@ -14,16 +14,31 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 @app.route('/', methods=['POST', 'GET'])
 def register():
     form = User_reg()
+    db_sess = db_session.create_session()
     if form.validate_on_submit():
         if form.password.data != form.repeat_password.data:
             return render_template('register.html', title='Регистрация',
                                    form=form,
                                    message="Пароли не совпадают")
+        user_log = db_sess.query(User).filter(User.login == form.login.data).first()
+        if user_log:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой логин уже существует!")
         user = User()
-        user.name = form.name.data
+        if form.name.data.isalpha() and form.surname.data.isalpha():
+            user.name = form.name.data.upper()
+            user.surname = form.surname.data.upper()
+        else:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Введите настоящую фамилию и имя!")
         user.login = form.login.data
         user.teacher = form.teacher.data
         user.hashed_password = hashlib.sha512(form.password.data.encode('utf-8')).hexdigest()
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/go')
     return render_template("register.html", form=form, title='Регистрация')
 
 
@@ -33,12 +48,13 @@ def authorize():
     if form.validate_on_submit():
         db_sess = db_session.create_session()
         user = db_sess.query(User).filter(User.login == form.login.data).first()
-        if user and hashlib.sha512(form.password.data.encode('utf-8')).hexdigest() == user.hashed_password:
-            login_user(user)
+        if user and \
+                hashlib.sha512(form.password.data.encode('utf-8')).hexdigest() \
+                == user.hashed_password:
             return redirect("/go")
-        return render_template('login.html',
+        return render_template('authorize.html',
                                message="Неправильный логин или пароль",
-                               form=form)
+                               form=form, title='Вход')
     return render_template("authorize.html", form=form, title='Вход')
 
 
@@ -48,4 +64,5 @@ def go():
 
 
 if __name__ == '__main__':
+    db_session.global_init("db/schools.db")
     serve(app, host='0.0.0.0', port=5000)
