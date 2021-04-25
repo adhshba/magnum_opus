bc83613301326dd23717b3108937d607d0355a10
arta159 <75035174+arta159@users.noreply.github.com>
2021-04-25 22:54:51 +0400

    Update main.py

--- main.py
+++ main.py
@@ -570,6 +570,6 @@ def logout():
 
 
 if __name__ == '__main__':
-    db_session.global_init("db/school11.db")
+    db_session.global_init("db/schools.db")
     # app.run(host='127.0.0.1', port=5000)
     serve(app, host='0.0.0.0', port=5000)
